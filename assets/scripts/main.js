a7baf2c8965383a43e5b04f5b55a45a182ad1185
Sara0112 <105769955+Sara0112@users.noreply.github.com>
2024-05-19 01:49:47 -0700

    fix main

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,12 @@
 
 // CONSTANTS
 const RECIPE_URLS = [
-  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
 ];
 
 // Run the init() function when the page has loaded
@@ -29,9 +29,9 @@ async function init() {
 }
 
 /**
-* Detects if there's a service worker, then loads it and begins the process
-* of installing it and getting it running
-*/
+ * Detects if there's a service worker, then loads it and begins the process
+ * of installing it and getting it running
+ */
 function initializeServiceWorker() {
   // EXPLORE - START (All explore numbers start with B)
   /*******************/
@@ -45,43 +45,46 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
-  
   if ('serviceWorker' in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
     self.addEventListener('load', () => {
+       // Steps B3-B6 will be *inside* the event listener's function created in B2
+        // B3. TODO - Register './sw.js' as a service worker (The MDN article
+        //            "Using Service Workers" will help you here)
       try {
+        // STEPS B6 ONWARDS WILL BE IN /sw.js
         const registration = navigator.serviceWorker.register("/sw.js", { scope: "/" });
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
         if (registration.waiting) {
-          console.log('service worker has been successfully registered');
+          console.log('the service worker has been successfully registered');
         }
+          // B5. TODO - In the event that the service worker registration fails, console
+          //            log that it has failed.
       } catch (error) {
-        console.log('the service worker registration fails');
+        console.log('the service worker registration failed');
       }
     });
   }
 }
 
 /**
-* Reads 'recipes' from localStorage and returns an array of
-* all of the recipes found (parsed, not in string form). If
-* nothing is found in localStorage, network requests are made to all
-* of the URLs in RECIPE_URLs, an array is made from those recipes, that
-* array is saved to localStorage, and then the array is returned.
-* @returns {Array<Object>} An array of recipes found in localStorage
-*/
+ * Reads 'recipes' from localStorage and returns an array of
+ * all of the recipes found (parsed, not in string form). If
+ * nothing is found in localStorage, network requests are made to all
+ * of the URLs in RECIPE_URLs, an array is made from those recipes, that
+ * array is saved to localStorage, and then the array is returned.
+ * @returns {Array<Object>} An array of recipes found in localStorage
+ */
 async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   /**************************/
+  let recipes = localStorage.getItem('recipes')
+  if (recipes != null) {
+    return JSON.parse(recipes);
+  }
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
@@ -113,14 +116,7 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
-
-  let recipes = localStorage.getItem('recipes')
-  if (recipes != null) {
-    return JSON.parse(recipes);
-  }
-
   recipes = [];
-
   return new Promise(async (resolve, reject) => {
     for (const url of RECIPE_URLS) {
       try {
@@ -140,21 +136,21 @@ async function getRecipes() {
 }
 
 /**
-* Takes in an array of recipes, converts it to a string, and then
-* saves that string to 'recipes' in localStorage
-* @param {Array<Object>} recipes An array of recipes
-*/
+ * Takes in an array of recipes, converts it to a string, and then
+ * saves that string to 'recipes' in localStorage
+ * @param {Array<Object>} recipes An array of recipes
+ */
 function saveRecipesToStorage(recipes) {
   localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
-* Takes in an array of recipes and for each recipe creates a
-* new <recipe-card> element, adds the recipe data to that card
-* using element.data = {...}, and then appends that new recipe
-* to <main>
-* @param {Array<Object>} recipes An array of recipes
-*/
+ * Takes in an array of recipes and for each recipe creates a
+ * new <recipe-card> element, adds the recipe data to that card
+ * using element.data = {...}, and then appends that new recipe
+ * to <main>
+ * @param {Array<Object>} recipes An array of recipes
+ */
 function addRecipesToDocument(recipes) {
   if (!recipes) return;
   let main = document.querySelector('main');
